d15ffe62290e374d0ac80b5d91cc7701743f9aeb
Rob <barrrobert84@gmail.com>
2023-09-06 17:44:08 +0200

    Searchbar refactoring

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { createCharacterCard } from "./components/card/card.js";
+import { createSearchBar } from './components/search-bar/search-bar.js'
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
+// const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
@@ -14,6 +15,10 @@ const pagination = document.querySelector('[data-js="pagination"]');
 let maxPage;
 let page = 1;
 let searchQuery = "";
+
+let searchBar = createSearchBar()
+
+
 
 searchBar.addEventListener("submit", (e) => {
   e.preventDefault();
