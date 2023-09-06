9ee4210561cc224e80c7d86f41ddb1651d0f0857
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-06 22:02:09 +0200

    change grid-template-columns for cards

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { createCharacterCard } from "./components/card/card.js";
-
 import { createSearchBar } from "./components/search-bar/search-bar.js";
 import { CreateButton } from "./components/nav-button/nav-button.js";
 
@@ -7,10 +6,7 @@ const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-// const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
-// const prevButton = document.querySelector('[data-js="button-prev"]');
-// const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
