39b9ef5a1624a6120b38b896fa577fe7ee83e6d7
Rob <barrrobert84@gmail.com>
2023-09-06 14:48:41 +0200

    Change searchQuery from const to let

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const pagination = document.querySelector('[data-js="pagination"]');
 // States
 let maxPage;
 const page = 23;
-const searchQuery = "";
+let searchQuery = "";
 
 
 prevButton.addEventListener("click", () => {
@@ -25,6 +25,9 @@ prevButton.addEventListener("click", () => {
 
 searchBar.addEventListener('submit', (e) => {
     e.preventDefault();
+    searchQuery = e.target.elements.query.value
+    fetchCharacters()
+    
     
 })
 
@@ -33,7 +36,7 @@ searchBar.addEventListener('submit', (e) => {
 async function fetchCharacters() {
   try {
     const response = await fetch(
-      `https://rickandmortyapi.com/api/character/?page=${page}`
+      `https://rickandmortyapi.com/api/character/?page=${page}/&name=${searchQuery}`
     );
 
     if (response.ok) {
