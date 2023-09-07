cc98814eab67f083cd398b9368b33c7b19ada6ea
Rob <barrrobert84@gmail.com>
2023-09-07 12:06:22 +0200

    More css changes and fixes

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-// const searchBar = document.querySelector('[data-js="search-bar"]');
+
 const navigation = document.querySelector('[data-js="navigation"]');
-// const prevButton = document.querySelector('[data-js="button-prev"]');
-// const nextButton = document.querySelector('[data-js="button-next"]');
-// const pagination = document.querySelector('[data-js="pagination"]');
+
 
 // States
 let maxPage = 1;
@@ -32,11 +30,13 @@ function searchChar(event) {
   console.log(searchQuery);
 
   cardContainer.innerHTML = "";
+  page = 1
   fetchCharacters();
 }
 
 async function fetchCharacters() {
   try {
+   
     const response = await fetch(
       `https://rickandmortyapi.com/api/character/?page=${page}&name=${searchQuery}`
     );
