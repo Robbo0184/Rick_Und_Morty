21e45666c781e7926a8b0587292d17660147c20c
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-08 00:17:39 +0200

    remove Alive duplicate text from a card

--- components/card/card.js
+++ components/card/card.js
@@ -15,11 +15,9 @@ export function createCharacterCard(props) {
 <div class="card__content">
   <h2 class="card__title">${name}</h2>
   <dl class="card__info">
-    <dt class="card__info-title">${status}</dt>
-    <dd class="card__info-description">Alive</dd>
-    <dt class="card__info-title">${type}</dt>
-    <dd class="card__info-description"></dd>
-    <dt class="card__info-title">${episode.length}</dt>
+    <dt class="card__info-status">${status}</dt>
+    <dt class="card__info-type">${type}</dt>
+    <dt class="card__info-episode">${episode.length}</dt>
   </dl>
 </div>
     `;
